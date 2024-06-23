779f90122a7c04d96851e89c974aadf87df88dd1
chengtf94 <chengtf94@qq.com>
2024-06-23 14:21:43 +0800

    read

--- example/src/main/java/io/netty/example/chengtf/study/heima/chat/message/AbstractResponseMessage.java
+++ example/src/main/java/io/netty/example/chengtf/study/heima/chat/message/AbstractResponseMessage.java
@@ -1,14 +1,26 @@
 package io.netty.example.chengtf.study.heima.chat.message;
 
-
 import io.netty.example.chengtf.study.heima._0common.protocol.Message;
 import lombok.Data;
 import lombok.ToString;
 
+/**
+ * 响应消息基类
+ *
+ * @author: chengtf
+ * @date: 2024/6/17
+ */
 @Data
 @ToString(callSuper = true)
 public abstract class AbstractResponseMessage extends Message {
+
+    /**
+     * 是否成功
+     */
     private boolean success;
+    /**
+     * 原因描述
+     */
     private String reason;
 
     public AbstractResponseMessage() {
@@ -18,4 +30,5 @@ public abstract class AbstractResponseMessage extends Message {
         this.success = success;
         this.reason = reason;
     }
+
 }
